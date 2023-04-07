5d979ded07a665d15c5e9856fe08909c88735312
Julianjuyo <je.oliverosf@uniandes.edu.co>
2023-04-07 14:20:41 -0500

    cambio restricción

--- backend-fastapi/schemas/indicator.py
+++ backend-fastapi/schemas/indicator.py
@@ -8,7 +8,7 @@ from pydantic import Field
 class Indicator(BaseModel):
 
     name: str = Field(..., min_length=1, example="RSI")
-    value: float = Field(..., gt=0, example=12212)
+    value: float = Field(..., example=12212)
     unix_time: int = Field(..., example=1673316847, gt=0)
 
     class Config:
@@ -18,7 +18,7 @@ class Indicator(BaseModel):
 class UpdateIndicator(BaseModel):
 
     name: Optional[str] = Field(None, min_length=1, example="RSI")
-    value: Optional[float] = Field(None, gt=0, example=12212)
+    value: Optional[float] = Field(None, example=12212)
     unix_time: Optional[int] = Field(None, example=1673316847, gt=0)
 
     class Config:
