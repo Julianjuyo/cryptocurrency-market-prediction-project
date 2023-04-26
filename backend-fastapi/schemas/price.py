95b0f54271988814bd2cdbbbf671454d459bd7d1
Julianjuyo <je.oliverosf@uniandes.edu.co>
2023-04-25 22:40:26 -0500

    cambios back para que reciba 0 en volumen

--- backend-fastapi/schemas/price.py
+++ backend-fastapi/schemas/price.py
@@ -9,12 +9,12 @@ from pydantic import Field
 
 class Price(BaseModel):
 
-    unix_time: int = Field(..., example=1673316847, gt=0)
-    open_price: float = Field(..., example=20000.2134, gt=0)
-    close_price: float = Field(..., example=22000.214, gt=0)
-    low_price: float = Field(..., example=19500.2, gt=0)
-    high_price: float = Field(..., example=22500.3, gt=0)
-    volume: float = Field(..., example=72.3, gt=0)
+    unix_time: int = Field(..., example=1673316847, ge=0,)
+    open_price: float = Field(..., example=20000.2134, ge=0)
+    close_price: float = Field(..., example=22000.214, ge=0)
+    low_price: float = Field(..., example=19500.2, ge=0)
+    high_price: float = Field(..., example=22500.3, ge=0)
+    volume: float = Field(..., example=72.3, ge=0)
     qav: float = Field(..., example=10)
     num_trades: int = Field(..., example=10)
     taker_base_vol: float = Field(..., example=10)
